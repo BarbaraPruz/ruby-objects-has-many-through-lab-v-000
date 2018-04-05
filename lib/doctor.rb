d1577631c7c0403c5abf0fac4f391e58c697f7d4
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-05 19:18:48 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,7 +12,10 @@ class Doctor
   #  binding.pry
     appointment.doctor = self   # just to make sure that all my appointments list me a Dr
     appointments.index(appointment) || appointments << appointment
-    patients.index(appointment.patient).uniq! if appointment.patient != nil
+    add_patient(appointment.patient) if appointment.patient != nil
   end
 
+  def add_patient(patient)
+    patients.index(patient) || patients << patient
+  end
 end
