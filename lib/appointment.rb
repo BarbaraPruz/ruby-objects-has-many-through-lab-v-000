0bc80132747ad45e44545c6d4776eecd42738a33
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-05 17:30:23 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,10 @@
 class Appointment
-   attr_accessor :date, :doctor 
+   attr_accessor :date, :doctor
 
    def initialize (date, doctor)
      @date = date
      @doctor = doctor
-   end 
-   
+     doctor.add_appointment(self)
+   end
+
 end
